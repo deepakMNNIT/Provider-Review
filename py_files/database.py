2e0a4829630737013de2a33f83dc2ae7e9b65d5f
deepak <34981370+deepakMNNIT@users.noreply.github.com>
2020-09-27 15:09:13 +0530

    Update database.py

--- py_files/database.py
+++ py_files/database.py
@@ -12,7 +12,27 @@ Base.query = db_session.query_property()
 
 def init_db():
     
-    from models import Provider_Review, Provider
+    from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, func
+    from sqlalchemy.orm import backref, relationship
+    
+    class Provider(Base):
+        __tablename__ = 'provider'
+        provider_id = Column(Integer, primary_key=True)
+        first_name = Column(String)
+        last_name = Column(String)
+    
+    
+    class Provider_Review(Base):
+        __tablename__ = 'provider_review'
+        id = Column(Integer, primary_key=True)
+        p_id = Column(Integer, ForeignKey('provider.provider_id'))
+        review = Column(String)
+        result = Column(String)
+        provider = relationship(
+            Provider,
+            backref=backref('provider',
+                            uselist=True,
+                            cascade='delete,all'))
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
 
